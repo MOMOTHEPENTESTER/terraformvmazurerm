1a32e805807191bcc2c2a2337c6d5b220130388e
gayathriparamasiva <160875055+MOMOTHEPENTESTER@users.noreply.github.com>
2024-11-30 00:27:48 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,27 +1,39 @@
-# azure virtual network 
-resource "azurerm_virtual_network" "terraformvnet" {
-  name                = "azurevnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.terraformdemo01.location
-  resource_group_name = azurerm_resource_group.terraformdemo01.name
-}
-
-# azure subnet
-resource "azurerm_subnet" "terraformsubnet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.terraformdemo01.name
-  virtual_network_name = azurerm_virtual_network.terraformvnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# allocating public ip
-resource "azurerm_public_ip" "terrapublicip" {
-  name                = "windowspublicip"
-  resource_group_name = azurerm_resource_group.terraformdemo01.name
-  location            = azurerm_resource_group.terraformdemo01.location
-  allocation_method   = "Static"
-
-  tags = {
-    environment = "Production"
+resource "azurerm_network_interface" "terra-demo" {
+  name                = "terra-demo-nic"
+  location            = azurerm_resource_group.terra-demo.location
+  resource_group_name = azurerm_resource_group.terra-demo.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.terra-demo.id
+    private_ip_address_allocation = "Dynamic"
+  #  public_ip_address_id=azurerm_public_ip.terra-demo.id
   }
 }
+
+resource "azurerm_linux_virtual_machine" "terra-demo" {
+  name                = "terra-demo-machine01"
+  resource_group_name = azurerm_resource_group.terra-demo.name
+  location            = azurerm_resource_group.terra-demo.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  admin_password      ="admin@123"
+  disable_password_authentication= "false"
+  network_interface_ids = [
+    azurerm_network_interface.terra-demo.id,
+  ]
+  #   admin_ssh_key {
+  #  username   = "adminuser"
+  #  admin_pa
+  #  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+ 
