ad09bf2e1c4a3e9e1e39f5ee16b14ce054f29617
gayathriparamasiva <160875055+MOMOTHEPENTESTER@users.noreply.github.com>
2024-11-30 00:28:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,24 +1,42 @@
-# azure provider source and version
+# We strongly recommend using the required_providers block to set the
+
+# Azure Provider source and version being used
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
+  required_providers {
+
+    azurerm = {
+
+      source  = "hashicorp/azurerm"
+
+      version ="=4.1.0"
+
+    }
+
+  }
+
+}
+ 
 # Configure the Microsoft Azure Provider
 
 provider "azurerm" {
-  subscription_id = "3ed31a29-541f-46c2-bbf2-e8d1db764848"
+
+  ## resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+
+   subscription_id = "3ed31a29-541f-46c2-bbf2-e8d1db764848"
+
   features {}
-}
 
-# azure resouce and location allocation
-
-resource "azurerm_resource_group" "terraformdemo01" {
-  name     = "rg-westeurope-01"
-  location = "West Europe"
 }
+ 
+# Create a resource group
+
+resource "azurerm_resource_group" "terra-demo" {
+
+  name     = "terra-demo-rg"
+
+  location = "Central India"
+
+}
+ 
